72afc178670b49c7af8a81c433ba6924c6a14d94
Sakamoto505 <93153773+Sakamoto505@users.noreply.github.com>
2022-11-18 01:02:06 +0300

    removed merge conflicts

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ApplicationRecord
+<<<<<<< HEAD
+=======
+  validates :title,:body, presence: true
+>>>>>>> parent of 80041db (Add answer spec)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_11_16_172325) do
+=======
+ActiveRecord::Schema.define(version: 2022_11_16_181010) do
+>>>>>>> parent of 80041db (Add answer spec)
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
