72a26072c30f97757a9e5e0ca8e5fd606e613ba0
Sakamoto505 <93153773+Sakamoto505@users.noreply.github.com>
2023-01-26 17:22:15 +0300

    Add new test for edit_answer

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-feature 'User can edit answer', %q{
+feature 'User can edit answer', "
   In order to correct mistake in answer
   As an authenticated user
   I'd like to be able to edit answer
-} do
-
+" do
   context 'Unauthenticated user' do
-    scenario "edits answer" do
+    scenario 'edits answer' do
       visit question_path(question)
       expect(page).to have_no_link('Edit answer')
     end
@@ -21,16 +22,28 @@ feature 'User can edit answer', %q{
     scenario 'edits his answer' do
       sign_in(user)
       visit question_path(question)
-    click_on 'Edit answer'
+      click_on 'Edit answer'
 
-    within '.answers' do
-      fill_in 'Your answer', with: 'edited answer'
-      click_on 'Save'
+      within '.answers' do
+        fill_in 'Your answer', with: 'edited answer'
+        click_on 'Save'
 
-      expect(page).to_not have_content answer.body
-      expect(page).to have_content 'edited answer'
-      expect(page).to_not have_selector 'textarea'
+        expect(page).to_not have_content answer.body
+        expect(page).to have_content 'edited answer'
+        expect(page).to_not have_selector 'textarea'
+      end
     end
+
+    scenario 'edits answer with errors' do
+      sign_in(user)
+      visit question_path(question)
+      click_on 'Edit answer'
+
+      within '.answers' do
+        fill_in 'Your answer', with: ''
+        click_on 'Save'
+      end
+        expect(page).to have_content "Body can't be blank"
     end
   end
 end
