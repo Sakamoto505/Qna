40d48cf964e844af1bbbb6b4562f913240dc2da4
Sakamoto505 <93153773+Sakamoto505@users.noreply.github.com>
2023-03-25 22:44:50 +0300

    Add Screencast

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -21,6 +21,16 @@ class AnswersController < ApplicationController
     @answer = @question.answers.new(answer_params)
     @answer.author = current_user
     @answer.save
+
+    respond_to do |format|
+      if @answer.save
+        format.json { render json: @answer }
+      else
+        format.json do
+          render json: @answer.errors.full_messages, status: :unprocessable_entity
+        end
+      end
+    end
   end
 
   def set_best
