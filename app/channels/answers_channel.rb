a7fae1c0aa882559b34292403f6da16cb0a0be47
Sakamoto505 <93153773+Sakamoto505@users.noreply.github.com>
2023-04-06 05:47:17 +0300

    Add channel for answer

--- app/channels/answers_channel.rb
+++ app/channels/answers_channel.rb
@@ -1,22 +1,5 @@
 class AnswersChannel < ApplicationCable::Channel
-  # def follow
-  #   stream_from "answer_#{params[:question_id]}"
-  # end
-
-
-  def receive(data)
-    ActionCable.server.broadcast(
-      "answer_#{params[:question_id]}",
-      render_answer(data)
-    )
-  end
-
-  private
-
-  def render_answer(answer)
-    ApplicationController.render(
-      partial: 'answers/answer',
-      locals: { answer: answer }
-    )
+  def follow
+    stream_from "answer_#{params[:question_id]}"
   end
 end
--- app/javascript/channels/answers_channel.js
+++ app/javascript/channels/answers_channel.js
@@ -1,17 +1,21 @@
-// import consumer from "./consumer"
-//
-// consumer.subscriptions.create({
-//   channel: "AnswersChannel",
-//   question_id: gon.question_id
-// }, {
-//   connected() {
-//     console.log('Answers channel connected.');
-//     this.perform('follow');
-//   },
-//
-//   received(data) {
-//     console.log('123123');
-//
-//     $('.answers').append(data);
-//   }
-// });
+import consumer from "./consumer"
+
+consumer.subscriptions.create({
+      channel: "AnswersChannel",
+      question_id: gon.question_id
+    }, {
+  connected() {
+    console.log('Answers channel connected.');
+    this.perform('follow');
+  },
+  received(data) {
+      console.log('fsfsdf');
+    if (gon.user_id === data.answer.author_id) return;
+
+    const template = require('./handlebars/answer.hbs');
+
+    data.is_question_owner = gon.user_id === gon.current_user_id;
+
+    $('.answers').append(template(data));
+  }
+});
