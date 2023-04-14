d9a26eadf399faf86bbca35984daec493b557be5
Sakamoto505 <93153773+Sakamoto505@users.noreply.github.com>
2023-04-14 06:18:27 +0300

    rubocop

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -9,14 +9,15 @@ class OauthCallbacksController < Devise::OmniauthCallbacksController
     oauth_authenticate('Google')
   end
 
-    private
+  private
+
   def oauth_authenticate(provider)
-  @user = User.find_for_oauth(request.env['omniauth.auth'])
-  if @user&.persisted?
-    sign_in_and_redirect @user, event: :authenticate
-    set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
-  else
-    redirect_to root_path, alert: 'Something went wrong'
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+    if @user&.persisted?
+      sign_in_and_redirect @user, event: :authenticate
+      set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
+    else
+      redirect_to root_path, alert: 'Something went wrong'
+    end
   end
-end
 end
