a465a3f57d497e688de2fe8776a14be00aafa00e
Sakamoto505 <93153773+Sakamoto505@users.noreply.github.com>
2023-04-14 06:09:51 +0300

    Add google oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:github]
+         :omniauthable, omniauth_providers: [:github, :google_oauth2]
 
   has_many :questions, class_name: 'Question', foreign_key: :author_id, dependent: :destroy
   has_many :answers, class_name: 'Answer', foreign_key: :author_id, dependent: :destroy
