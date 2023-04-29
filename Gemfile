520a58164acda4078bf2dfff85bdbabf00ef7993
Sakamoto505 <93153773+Sakamoto505@users.noreply.github.com>
2023-04-30 01:19:42 +0300

    Add profiles

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ gem 'bootsnap', require: false
 # gem "image_processing", "~> 1.2"
 gem 'cocoon'
 gem 'devise'
+gem 'doorkeeper'
 gem 'gon'
 gem 'handlebars-source'
 gem 'omniauth'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  use_doorkeeper
   get 'rewards/index'
   devise_for :users, controllers: { omniauth_callbacks: 'oauth_callbacks' }
 
@@ -29,4 +30,12 @@ Rails.application.routes.draw do
   resources :rewards, only: :index
 
   mount ActionCable.server => '/cable'
+
+  namespace :api do
+    namespace :v1 do
+      resource 'profiles', only: [] do
+        get :me, on: :collection
+      end
+    end
+  end
 end
