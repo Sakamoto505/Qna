6b70de4ec79192ace794618fb02aaa466d6e63a2
Sakamoto505 <93153773+Sakamoto505@users.noreply.github.com>
2023-06-15 20:43:09 +0300

    F

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,7 +99,9 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = { host: '127.0.0.1' }
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
-  #
+
+
+
   #   address: 'smtp.gmail.com',
   #   port: 587,
   #   user_name:            Rails.application.credentials[:production][:gmail][:email],
