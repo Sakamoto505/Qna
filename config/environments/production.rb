c93144cb08774398698782b08b88369e09582157
Sakamoto505 <93153773+Sakamoto505@users.noreply.github.com>
2023-06-15 19:19:12 +0300

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,14 +93,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: Rails.application.credentials[Rails.env.to_sym][:gmail][:email],
-    password: Rails.application.credentials[Rails.env.to_sym][:gmail][:password],
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.perform_caching = false
+  # config.action_mailer.default_url_options = { host: '127.0.0.1' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #
+  #   address: 'smtp.gmail.com',
+  #   port: 587,
+  #   user_name:            Rails.application.credentials[:production][:gmail][:email],
+  #   password:             Rails.application.credentials[:production][:gmail][:password],
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
 end
